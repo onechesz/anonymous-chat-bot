62081a9fc7fcf4266954f668f995b23543d314c5
Ivan Minyaev <i.onechesz@gmail.com>
2026-05-31 20:27:54 +0300

    Spring Initializr

---
+++ src/main/java/com/ivanminyaev/anonymous_chat_bot/AnonymousChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.ivanminyaev.anonymous_chat_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousChatBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanminyaev/anonymous_chat_bot/AnonymousChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanminyaev.anonymous_chat_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
